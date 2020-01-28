d98c142b514e53e6fc92ff8025a706ec73d1808f
CONDOMITTI <nicolas.condomitti@etu.univ-lyon1.fr>
2020-01-28 19:10:06 +0100

    CA MARCHE

--- Web_server/server_js.js
+++ Web_server/server_js.js
@@ -3,8 +3,17 @@ const http       = require("http");
 const express    = require('express');
 const bodyParser = require('body-parser');
 const app        = express();
+const fs         = require("fs");
 
 const port = 8880
+
+var lampStatus = 0;
+
+const parameters = {
+    Y_Gamer : 0,
+    Ymin_Gap : 0,
+    Ymax_Gap : 0
+}
 
 // const wss = new WebSocket.Server({ server });
 
@@ -14,8 +23,36 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 app.post('/', (req, res) => {
-    console.log("request received !\n"+req.body)
-    res.send("salut !");
+    // console.log("RECU :\n"+ JSON.stringify(req.body));
+    // res.send(parameters);
+    if(req.body.changeLamp) {
+        lampStatus = req.body.changeLamp;
+        console.log("lampChanged");
+        res.send();
+        return;
+    }
+    if(req.body.lampStatus == 1) {
+        res.json({lampStatus : lampStatus});
+        return;
+    }
+    // console.log("request received !\n" + req.body.GETPOSITION);
+    if(req.body.GETPOSITION == 1) {
+        // console.log("request received !\n" + req.body.GETPOSITION);
+        // res.set('Content-Type', 'application/json');
+        res.json(parameters);
+    }
+    // else if(req.body.GETPOSITION == 0) {
+    //     lampStatus = req.bodys.LIGHTUP;
+    // }
+});
+
+app.get('/', function (req, res) {
+    //arduino.write("255,125,000");
+    fs.readFile('./jeu.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
 
 
